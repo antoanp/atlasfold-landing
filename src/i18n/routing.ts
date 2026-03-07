035a682ef96babb0014ae6efbd59e643edd0793f
Antoan Parashkevov <antoan.webdev@gmail.com>
2026-03-07 13:30:58 +0200

    Always append the locale prefix

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,7 @@
-import { defineRouting } from 'next-intl/routing';
+import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ['bg', 'en'],
-  defaultLocale: 'bg',
-  localePrefix: 'as-needed',
+  locales: ["bg", "en"],
+  defaultLocale: "bg",
+  localePrefix: "always",
 });
