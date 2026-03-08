92843d8c8a34d6ecb99db66191d544eb5457b897
Antoan Parashkevov <antoan.webdev@gmail.com>
2026-03-08 22:43:09 +0200

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,6 +7,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${baseUrl}/bg`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
       alternates: {
         languages: { bg: `${baseUrl}/bg`, en: `${baseUrl}/en` },
       },
@@ -14,6 +16,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${baseUrl}/en`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
       alternates: {
         languages: { bg: `${baseUrl}/bg`, en: `${baseUrl}/en` },
       },
